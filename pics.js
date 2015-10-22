98d77b96ac735566b75736bf27f13586d7b24e12
Ariel <ariel.steele@west-mec.org>
2015-10-22 15:25:15 -0700

    I changed some of the CSS, added id's to all of the images, and added javascript/jquery to be able to change the picture when clicking on the picture.

--- pics.js
+++ pics.js
@@ -1,3 +1,26 @@
 /**
  * Created by session1 on 10/22/15.
  */
+$(document).ready(function(){
+    $('#img1').click(function() {
+        $('#img1').attr('src','/Data-Graph-Website-Part-2-/images/employment.png');
+    })
+});
+
+    $(document).ready(function(){
+        $('#img2').click(function() {
+            $('#img2').attr('src','/Data-Graph-Website-Part-2-/images/mostcommoninchildren.png');
+        })
+    });
+
+        $(document).ready(function(){
+            $('#img3').click(function() {
+                $('#img3').attr('src','/Data-Graph-Website-Part-2-/images/byAge.png');
+            })
+        });
+
+            $(document).ready(function(){
+                $('#img4').click(function() {
+                    $('#img4').attr('src','/Data-Graph-Website-Part-2-/images/specificDisabilities.png');
+                })
+            });
