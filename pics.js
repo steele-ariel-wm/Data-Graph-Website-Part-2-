46ee68551036031fe09677167e6342e797a22e31
Ariel <ariel.steele@west-mec.org>
2015-10-23 15:14:51 -0700

    I adjusted the size of the images a little bit, I added the images I needed to add in the html, and I made them all clickable. I also added where it will click back and forth, not just one time.

--- pics.js
+++ pics.js
@@ -3,24 +3,92 @@
  */
 $(document).ready(function(){
     $('#img1').click(function() {
-        $('#img1').attr('src','/Data-Graph-Website-Part-2-/images/employment.png');
-    })
-});
+        var current1 = $('#img1').attr('src');
 
-    $(document).ready(function(){
-        $('#img2').click(function() {
-            $('#img2').attr('src','/Data-Graph-Website-Part-2-/images/mostcommoninchildren.png');
-        })
+        if(current1 == '/Data-Graph-Website-Part-2-/images/employment.png'){
+            $('#img1').attr('src', 'http://static.guim.co.uk/sys-images/Guardian/Pix/pictures/2011/4/12/1302621170311/woman-working-in-wood-sho-004.jpg');
+        }else {
+            $('#img1').attr('src', '/Data-Graph-Website-Part-2-/images/employment.png');
+        }
     });
 
-        $(document).ready(function(){
-            $('#img3').click(function() {
-                $('#img3').attr('src','/Data-Graph-Website-Part-2-/images/byAge.png');
-            })
-        });
+    $('#img2').click(function() {
+        var current2 = $('#img2').attr('src');
 
-            $(document).ready(function(){
-                $('#img4').click(function() {
-                    $('#img4').attr('src','/Data-Graph-Website-Part-2-/images/specificDisabilities.png');
-                })
-            });
+        if(current2 == '/Data-Graph-Website-Part-2-/images/mostcommoninchildren.png'){
+            $('#img2').attr('src', 'http://www.yai-rc.org/curricula/img/1008-5801.gif');
+        }else {
+            $('#img2').attr('src', '/Data-Graph-Website-Part-2-/images/mostcommoninchildren.png');
+        }
+    });
+
+    $('#img3').click(function() {
+        var current3 = $('#img3').attr('src');
+
+        if(current3 == '/Data-Graph-Website-Part-2-/images/byAge.png'){
+            $('#img3').attr('src', 'http://ldaamerica.org/wp-content/uploads/2013/08/group-of-people.jpg');
+        }else {
+            $('#img3').attr('src', '/Data-Graph-Website-Part-2-/images/byAge.png');
+        }
+    });
+
+    $('#img4').click(function() {
+        var current4 = $('#img4').attr('src');
+
+        if(current4 == '/Data-Graph-Website-Part-2-/images/specificDisabilities.png'){
+            $('#img4').attr('src', 'http://ih.constantcontact.com/fs015/1101828445774/img/383.jpg?a=1111353578837');
+        }else {
+            $('#img4').attr('src', '/Data-Graph-Website-Part-2-/images/specificDisabilities.png');
+        }
+    });
+
+    $('#img5').click(function() {
+        var current5 = $('#img5').attr('src');
+
+        if(current5 == 'http://www.themoscowtimes.com/upload/iblock/e26/5348-04-2014_Winter_Olympics_opening_ceremony_m2014-02-07x_08.jpg'){
+            $('#img5').attr('src', 'http://alba.sportabili.org/files/u1/IMG_1667__Large__0.jpg');
+        }else {
+            $('#img5').attr('src', 'http://www.themoscowtimes.com/upload/iblock/e26/5348-04-2014_Winter_Olympics_opening_ceremony_m2014-02-07x_08.jpg');
+        }
+    });
+
+    $('#img6').click(function() {
+        var current6 = $('#img6').attr('src');
+
+        if(current6 == 'http://www.picenotime.it/uploads/grandi/1395399756132797.jpg'){
+            $('#img6').attr('src','http://www.fidal.it/upload/old_images/OMG307.jpg');
+        }else {
+            $('#img6').attr('src', 'http://www.picenotime.it/uploads/grandi/1395399756132797.jpg');
+        }
+    });
+
+    $('#img7').click(function() {
+        var current7 = $('#img7').attr('src');
+
+        if(current7 == 'http://pzsnstart.eu/wp-content/uploads/2014/11/natalia_partyka.jpg'){
+            $('#img7').attr('src', 'http://resources3.news.com.au/images/2012/07/29/1226437/800543-natalia-partyka.jpg');
+        }else {
+            $('#img7').attr('src', 'http://pzsnstart.eu/wp-content/uploads/2014/11/natalia_partyka.jpg');
+        }
+    });
+
+    $('#img8').click(function() {
+        var current8 = $('#img8').attr('src');
+
+        if(current8 == 'http://media.olympic.org.nz/sites/media/files/museum/fairhall-ner-object-4-0.jpg'){
+            $('#img8').attr('src', 'http://megamocni.pl/img/content/userfiles/61/images/neroli_fairhall.jpg');
+        }else {
+            $('#img8').attr('src', 'http://media.olympic.org.nz/sites/media/files/museum/fairhall-ner-object-4-0.jpg');
+        }
+    });
+
+    $('#img9').click(function() {
+        var current9 = $('#img9').attr('src');
+
+        if(current9 == 'http://pctrs.network.hu/clubpicture/9/_/szekeres_pal_vivas_90696_346907_n.jpg'){
+            $('#img9').attr('src', 'http://www.chinadaily.com.cn/paralympics/images/attachement/jpg/site1/20080915/000d60aa06df0a38080717.jpg');
+        }else {
+            $('#img9').attr('src', 'http://pctrs.network.hu/clubpicture/9/_/szekeres_pal_vivas_90696_346907_n.jpg');
+        }
+    });
+});
